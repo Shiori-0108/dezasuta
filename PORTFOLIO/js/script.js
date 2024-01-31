3cc82e6f0a97b4ac1ca78e7feacd29adec3f096b
Shiori-0108 <skmr0108@gmail.com>
2024-01-31 22:16:57 +0900

    0131ポートフォリオ

--- PORTFOLIO/js/script.js
+++ PORTFOLIO/js/script.js
@@ -1,9 +1,36 @@
-$(function () {
     
-    //ハンバーガー
+//ハンバーガー
+$(function () {
+
     $(".sp_btn, .sp_nav li").on('click', function () {
         $('.sp_nav').fadeToggle();
         $('.sp_btn').toggleClass('open');
     });
 
 });
+
+
+// モーダルウィンドウ
+$(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#closeModal , #modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+    
+    $('#openModal2').click(function(){
+        $('#modalArea2').fadeIn();
+    });
+    $('#closeModal2 , #modalBg2').click(function(){
+      $('#modalArea2').fadeOut();
+    });
+    
+      $('#openModal3').click(function(){
+        $('#modalArea3').fadeIn();
+    });
+    $('#closeModal3 , #modalBg3').click(function(){
+      $('#modalArea3').fadeOut();
+    });
+    
+  });
